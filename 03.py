01fa3ac3ec73a8694c712886e222f8fe1d12be27
jaozinho nonato <gabriel282828@outlook.com>
2025-11-18 08:47:17 -0300

    Add files via upload

---
+++ 03.py
@@ -0,0 +1,7 @@
+n = int(input("Digite um número"))
+c = 0
+
+for i in range(0,n):
+    c+=n
+
+print(c)
---
+++ 04.py
@@ -0,0 +1,5 @@
+n = int(input("Digite um número: "))
+c = 0
+for i in range(1,n+1):
+    c+=i
+print("A soma de 1 até", n ,"é", c ,".")
